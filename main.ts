9701b26b54e9f529e0c63bf37f4497b0d4e96c00
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2023-12-15 11:01:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    hunger += 1
+    basic.showNumber(hunger)
+})
+input.onButtonPressed(Button.B, function () {
+    hunger = 0
+    basic.showNumber(hunger)
+})
+let hunger = 0
+hunger = 0
